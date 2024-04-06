92443441ceefae6645ad19d57ff5203fff4e3963
haouit <22520404@gm.uit.edu.vn>
2024-04-06 07:42:11 +0700

    init svelte

---
+++ Frontend/src/routes/Counter.svelte
@@ -0,0 +1,175 @@
+<script>
+	import { spring } from 'svelte/motion';
+	import dog from '$lib/images/dog.jpg';
+	import cat from '$lib/images/cat.jpg';
+	import chinchilla from '$lib/images/chinchilla.jpg';
+	let count = 0;
+
+	const displayed_count = spring();
+	$: displayed_count.set(count);
+	$: offset = modulo($displayed_count, 1);
+
+	/**
+	 * @param {number} n
+	 * @param {number} m
+	 */
+	function modulo(n, m) {
+		// handle negative numbers
+		return ((n % m) + m) % m;
+	}
+</script>
+
+<!-- <div class="counter">
+	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
+		<svg aria-hidden="true" viewBox="0 0 1 1">
+			<path d="M0,0.5 L1,0.5" />
+		</svg>
+	</button>
+
+	<div class="counter-viewport">
+		<div class="counter-digits" style="transform: translate(0, {100 * offset}%)">
+			<strong class="hidden" aria-hidden="true">{Math.floor($displayed_count + 1)}</strong>
+			<strong>{Math.floor($displayed_count)}</strong>
+		</div>
+	</div>
+
+	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+		<svg aria-hidden="true" viewBox="0 0 1 1">
+			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
+		</svg>
+	</button>
+</div> -->
+
+<!-- Pet detail-->
+<table>
+	<tr>
+		<td colspan="3" id="select-pet">Chọn thú cưng</td>
+	</tr>
+	<div id="petdetail">
+		<tr>
+			<td>
+				<img src={dog} alt="Norway"/>
+			</td>
+			<td>
+				<img src={chinchilla} alt="Norton" />
+			</td>
+			<td>
+				<img src={cat} alt="Norman" />
+			</td>
+		</tr>
+
+		<!--mo ta -->
+		<tr>
+			<td>Chó con</td>
+			<td>Chinchilla</td>
+			<td>Mèo lười</td>
+		</tr>
+	</div>
+
+	<div style="display: flex; justify-content: center; margin: 40px;" class="select">
+		<button
+			type="button"
+			style="background-color: #f5dea8; color: brown; padding:6px; border: 1px solid brown; border-radius: 10px">
+			<a href="petcare.html">Chọn nuôi</a>
+		</button>
+	</div>
+</table>
+
+<!-- 
+<style>
+	.counter {
+		display: flex;
+		border-top: 1px solid rgba(0, 0, 0, 0.1);
+		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+		margin: 1rem 0;
+	}
+
+	.counter button {
+		width: 2em;
+		padding: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: 0;
+		background-color: transparent;
+		touch-action: manipulation;
+		font-size: 2rem;
+	}
+
+	.counter button:hover {
+		background-color: var(--color-bg-1);
+	}
+
+	svg {
+		width: 25%;
+		height: 25%;
+	}
+
+	path {
+		vector-effect: non-scaling-stroke;
+		stroke-width: 2px;
+		stroke: #444;
+	}
+
+	.counter-viewport {
+		width: 8em;
+		height: 4em;
+		overflow: hidden;
+		text-align: center;
+		position: relative;
+	}
+
+	.counter-viewport strong {
+		position: absolute;
+		display: flex;
+		width: 100%;
+		height: 100%;
+		font-weight: 400;
+		color: var(--color-theme-1);
+		font-size: 4rem;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.counter-digits {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+
+	.hidden {
+		top: -100%;
+		user-select: none;
+	}
+</style> -->
+
+<style>
+	table {
+		width: 100%;
+		border-collapse: collapse;
+		border-radius: 13px;
+		background-color: #ffcdac;
+	}
+	#select-pet {
+		display: flex;
+		justify-content: center;
+		margin: 40p;
+		font-family: Calibri;
+		font-size: 50px;
+		color: rgb(233, 107, 23);
+	}
+	td {
+		padding: 8px;
+		text-align: center;
+		font-family: Calibri;
+		font-size: 30px;
+		color: rgb(233, 107, 23);
+	}
+	td>img {
+		border-radius: 10px;	
+	}
+
+	td>img:hover {
+		background-color: #ff6600;
+	}
+</style>
---
+++ Frontend/src/routes/Header.svelte
@@ -0,0 +1,218 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/logo.png';
+</script>
+
+<!-- 
+<header>
+	<div class="corner">
+		<a href="https://kit.svelte.dev">
+			<img src={logo} alt="SvelteKit" />
+		</a>
+	</div>
+
+	<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">About</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+				<a href="/sverdle">Sverdle</a>
+			</li>
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
+
+	<div class="corner">
+		<a href="https://github.com/sveltejs/kit">
+			<img src={github} alt="GitHub" />
+		</a>
+	</div>
+</header> -->
+
+<header>
+	<!--Thanh điều hướng-->
+	<div class="menu">
+		<nav>
+			<ul>
+				<div style="padding:0">
+					<li>
+						<a href="Home.html"> <img src={logo} alt="logo" style="height:65px; width:60px" /></a>
+					</li>
+				</div>
+				<li class="menu_list"><a href="#petdetail">Pet Detail</a></li>
+				<li class="menu_list"><a href="petcare.html">Pet care</a></li>
+				<li class="menu_list"><a href="#group">Forum</a></li>
+				<div>
+					<ul class="top_icon">
+						<li class="login"><a href="SignIn.html">Login</a></li>
+
+						<li class="signup"><a href="SignUp.html">Signup</a></li>
+					</ul>
+				</div>
+			</ul>
+		</nav>
+	</div>
+</header>
+
+<!-- 
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	svg {
+		width: 2em;
+		height: 3em;
+		display: block;
+	}
+
+	path {
+		fill: var(--background);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--color-theme-1);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style> -->
+
+<style>
+	header {
+		background-color: #f29f6e;
+	}
+	div.menu {
+		display: grid;
+		width: 120px;
+		text-align: center;
+		display: inline;
+		position: sticky;
+		top: 0;
+		left: 0;
+		right: 0;
+	}
+
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+	}
+
+	li {
+		float: left;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 30px;
+	}
+	
+	li a:hover {
+		background-color: #ff6600;
+	}
+
+	li.menu_list a {
+		display: block;
+		color: white;
+		text-align: center;
+		padding: 15px 25px;
+		text-decoration: none;
+	}
+
+	li.login a {
+		display: flex;
+		color: white;
+		text-align: center;
+		padding: 15px 20px;
+		text-decoration: none;
+	}
+
+	li.signup a {
+		display: flex;
+		color: white;
+		text-align: center;
+		padding: 15px;
+		padding-left: 35px;
+		padding-right: 35px;
+		text-decoration: none;
+	}
+
+	ul.top_icon {
+		float: right;
+	}
+</style>
