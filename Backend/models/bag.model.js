95c58d2cd539033557ac94bd45d4dcc1daf2c933
PhanQDat <22520233@gm.uit.edu.vn>
2024-05-16 11:54:05 +0700

    bag_user

---
+++ Backend/models/bag.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');	
+
+const bagSchema = mongoose.Schema({
+	itemId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Item',
+		required: true
+	},
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	name: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	quantity: {
+		type: Number,
+		required: true
+	}
+},
+{
+	timestamps: true
+});
+
+const Bag = mongoose.model('bags', bagSchema);
+
+module.exports = Bag;
