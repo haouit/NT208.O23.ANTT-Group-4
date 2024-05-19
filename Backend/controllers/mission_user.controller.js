25ccdeadcd3a92919e7eb91fa090f688782725b5
PhanQDat <22520233@gm.uit.edu.vn>
2024-05-20 02:00:06 +0700

    fix route

--- Backend/controllers/mission_user.controller.js
+++ Backend/controllers/mission_user.controller.js
@@ -2,7 +2,7 @@ const Mission = require('../models/mission.model');
 const Mission_User = require('../models/mission_user.model');
 const User = require('../models/user.model');
 
-const getAllMission_Users = async (req, res) => {
+const getAllMission_users = async (req, res) => {
     try {
         const mission_users = await Mission_User.find();
         res.status(200).json(mission_users);
@@ -11,7 +11,7 @@ const getAllMission_Users = async (req, res) => {
     }
 };
 
-const getMission_UserById = async (req, res) => {
+const getMission_userById = async (req, res) => {
     try {
         const mission_user = await Mission_User.findById(req.params.id);
         res.status(200).json(mission_user);
@@ -21,7 +21,7 @@ const getMission_UserById = async (req, res) => {
     }
 }
 
-const createMission_User = async (req, res) => {
+const createMission_user = async (req, res) => {
     try {
         const mission_user = await Mission_User.create({
             missionId: req.body.missionId,
@@ -36,7 +36,7 @@ const createMission_User = async (req, res) => {
 }
 
 module.exports = {
-    getAllMission_Users,
-    getMission_UserById,
-    createMission_User
+    getAllMission_users,
+    getMission_userById,
+    createMission_user
 };
