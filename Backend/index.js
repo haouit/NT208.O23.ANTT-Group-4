551810c88679008170964cc89bab9cec973e59a2
PhanQDat <22520233@gm.uit.edu.vn>
2024-05-20 01:54:02 +0700

    update BE/index

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 
 const userRouter = require('./routers/user.route');
 const billRouter = require('./routers/bill.route');
+const mission_userRouter = require('./routers/mission_user.route');
+const pet_ownerRouter = require('./routers/pet_owner.route');
+const achieverRouter = require('./routers/achieve.route');
 
 const urlConnectionData = fs.readFileSync('url_connection.json', 'utf8');
 const urlConnection = JSON.parse(urlConnectionData);
@@ -20,6 +23,9 @@ app.use(cors());
 // routes
 app.use('/api/users', userRouter);
 app.use('/api/bills', billRouter);
+app.use('/api/mission_users', mission_userRouter);
+app.use('/api/pet_owners', pet_ownerRouter);
+app.use('/api/achieves', achieverRouter);
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
