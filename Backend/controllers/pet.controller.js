ec4d278ceb86316eb87e56505338b09c2bff5126
haouit <22520404@gm.uit.edu.vn>
2024-05-31 12:29:24 +0700

    create api for exp up

--- Backend/controllers/pet.controller.js
+++ Backend/controllers/pet.controller.js
@@ -1,4 +1,5 @@
 const Pet = require('../models/pet.model');
+const Bag = require('../models/bag.model');
 
 const petPet = async (req, res) => {
 	try {
@@ -31,7 +32,51 @@ const checkPet = async (req, res) => {
 	}
 }
 
+const expUp = async (req, res) => {
+	try {
+		const userId = req.body.userId;
+		// add exp
+		if (req.body.foodRequire != -1) {
+			const bag = await Bag.findOne({
+				userId
+			});
+			if (bag) {
+				const id = ['663e0d50adbede46c0bd88bb', '663e0da1adbede46c0bd88bd', '663e0d8fadbede46c0bd88bc'];
+				const foodId = id[req.body.foodRequire];
+				const food = bag.userItems.find(item => item.itemId == foodId);
+				if (!food) {
+					res.status(400).json({ message: 'Insufficient food' });
+					return;
+				}
+				if (food.quantity == 0) {
+					res.status(400).json({ message: 'Insufficient food' });
+					return;
+				}
+				// update bag quantity
+				await Bag.findOneAndUpdate(
+					{ userId: req.body.userId },
+					{ $inc: { "userItems.$[elem].quantity": -1 } },
+					{ arrayFilters: [{ "elem.itemId": foodId }] }
+				);
+			} else {
+				res.status(400).json({ message: 'Insufficient food' });
+				return;
+			}
+		}
+		const pet = await Pet.findOne({
+			userId: req.body.userId
+		});
+		pet.exp += req.body.exp;
+		await pet.save();
+		res.status(200).json(pet);
+	}
+	catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+}
+
 module.exports = {
 	petPet,
-	checkPet
+	checkPet,
+	expUp
 };
--- Backend/routers/pet.route.js
+++ Backend/routers/pet.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-const { petPet, checkPet } = require('../controllers/pet.controller');
+const { petPet, checkPet, expUp } = require('../controllers/pet.controller');
 
 router.post('/add', petPet);
 router.get('/check/:userId', checkPet);
+router.post('/exp', expUp);
 
 module.exports = router;
