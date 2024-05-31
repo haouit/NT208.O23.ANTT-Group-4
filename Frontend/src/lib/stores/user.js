c30c0800180c4d6df9da6e8fdd94e6290d8fd760
haouit <22520404@gm.uit.edu.vn>
2024-05-31 12:27:51 +0700

    fix login sometime working wrong

--- Frontend/src/lib/stores/user.js
+++ Frontend/src/lib/stores/user.js
@@ -4,7 +4,7 @@ import { URL_API, cookie } from "./checkToken";
 const loggedIn = writable();
 
 async function getUser() {
-	const id = localStorage.getItem('id');
+	const id = localStorage.getItem('id') || undefined;
 	const response = await fetch(`${URL_API}/api/users/id/${id}`, {
 		method: 'GET',
 		headers: {
@@ -25,6 +25,7 @@ async function getUser() {
 async function LogOut() {
 	localStorage.removeItem('id');
 	document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+	loggedIn.set(false);
 	fetch(`${URL_API}/api/users/logout`, {
 		method: 'POST',
 		headers: {
