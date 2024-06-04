c0d8149267da7a75a870e5e4a5ada533e638f0a2
haouit <22520404@gm.uit.edu.vn>
2024-06-04 16:35:18 +0700

    use svelte handler after build

--- Backend/index.js
+++ Backend/index.js
@@ -2,51 +2,58 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const fs = require('fs');
 
-const { requireAuth } = require('./middleware/auth');
+let handler;
 
-const userRouter = require('./routers/user.route');
-const billRouter = require('./routers/bill.route');
-const petRouter = require('./routers/pet.route');
-const mission_userRouter = require('./routers/mission_user.route');
-const pet_ownerRouter = require('./routers/pet_owner.route');
-const achieverRouter = require('./routers/achieve.route');
+import('../Frontend/build/handler.js').then(module => {
+	const handler = module.handler;
+  
 
-const app = express();
+	const { requireAuth } = require('./middleware/auth');
 
-// Middleware
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors());
+	const userRouter = require('./routers/user.route');
+	const billRouter = require('./routers/bill.route');
+	const petRouter = require('./routers/pet.route');
+	const mission_userRouter = require('./routers/mission_user.route');
+	const pet_ownerRouter = require('./routers/pet_owner.route');
+	const achieverRouter = require('./routers/achieve.route');
+	
+	const app = express();
+	
+	// Middleware
+	app.use(express.json());
+	app.use(cookieParser());
+	app.use(cors());
+	
+	// routes
+	app.use('/api/users', userRouter);
+	app.use('/api/bills', billRouter);
+	app.use('/api/pets', petRouter);
+	app.use('/api/mission_users', mission_userRouter);
+	app.use('/api/pet_owners', pet_ownerRouter);
+	app.use('/api/achieves', achieverRouter);
+	
+	app.get('/protected', requireAuth, (req, res) => {
+		res.send('You are authenticated!');
+	});
 
-// routes
-app.use('/api/users', userRouter);
-app.use('/api/bills', billRouter);
-app.use('/api/pets', petRouter);
-app.use('/api/mission_users', mission_userRouter);
-app.use('/api/pet_owners', pet_ownerRouter);
-app.use('/api/achieves', achieverRouter);
+	// let SvelteKit handle everything else, including serving prerendered pages and static assets
+	app.use(handler);
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
-app.get('/protected', requireAuth, (req, res) => {
-	res.send('You are authenticated!');
-});
-
-
-app.listen(5038, () => {
-	console.log('Server started on http://localhost:5038');
-});
-
-mongoose.connect(process.env.MONGO_URI)
-	.then(() => {
-		console.log('Connected to MongoDB');
-	})
-	.catch((error) => {
-		console.log('Connection to MongoDB failed');
-		console.log(error);
+	app.listen(5038, () => {
+		console.log('Server started on http://localhost:5038');
 	});
 	
+	mongoose.connect(process.env.MONGO_URI)
+		.then(() => {
+			console.log('Connected to MongoDB');
+		})
+		.catch((error) => {
+			console.log('Connection to MongoDB failed');
+			console.log(error);
+		});
+		
+  
+  }).catch(err => {
+	console.error(err);
+  });
