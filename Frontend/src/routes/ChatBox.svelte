3bea599764b23ec148990d97947b99f3eb382bda
haouit <22520404@gm.uit.edu.vn>
2024-06-04 14:51:53 +0700

    component ChatBox for chatting

---
+++ Frontend/src/routes/ChatBox.svelte
@@ -0,0 +1,120 @@
+<script>
+	import loader from '$lib/images/loader.gif';
+	import { URL_API } from '$lib/stores/checkToken';
+
+	$: isLoaderVisible = false;
+	$: chatHistory = [{ sender: 'bot', message: 'Chào bạn! Graw Graw!' }];
+	$: userMessage = '';
+
+	async function sendMessage() {
+		const message = userMessage.trim();
+		userMessage = ''; // Clear the input field
+		chatHistory = [...chatHistory, { sender: 'user', message }];
+		try {
+			const response = await fetch(`${URL_API}/api/pets/chat`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ userInput: message })
+			});
+
+			const data = await response.json();
+			const botMessage = data.response;
+			// Add chat message to the chat history
+			chatHistory = [...chatHistory, { sender: 'bot', message: botMessage }];
+		} catch (error) {
+			console.error('Error:', error);
+		}
+	}
+
+	function submitForm() {
+		isLoaderVisible = true; // Show the loader
+		if (userMessage.trim() === '') return; // Don't send empty message
+		sendMessage().finally(() => {
+			isLoaderVisible = false; // Hide the loader after the message is sent
+		});
+	}
+</script>
+
+<div id="chat-container">
+	<h1>Chat with pet</h1>
+	<div id="chat-history">
+		{#each chatHistory as message}
+			<div class="{message.sender}-message">{message.message}</div>
+		{/each}
+		{#if isLoaderVisible}
+			<div id="loader">
+				<img src={loader} width="100px" alt="Loading..." />
+			</div>
+		{/if}
+	</div>
+
+	<form id="chat-form" on:submit|preventDefault={submitForm}>
+		<input type="text" bind:value={userMessage} id="user-input" placeholder="Enter your message" />
+		<button type="submit">Send</button>
+	</form>
+</div>
+
+<style>
+	#chat-container {
+		background-color: #fff;
+		border-radius: 10px;
+		padding: 20px;
+		box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+		width: 400px;
+	}
+
+	h1 {
+		text-align: center;
+		margin-bottom: 20px;
+	}
+
+	#chat-history {
+		height: 300px;
+		overflow-y: scroll;
+	}
+
+	.user-message {
+		text-align: right;
+		padding: 10px;
+		background-color: #f0f0f0;
+		border-radius: 10px;
+		margin-bottom: 5px;
+	}
+
+	.bot-message {
+		text-align: left;
+		padding: 10px;
+		background-color: #e0f0e0;
+		border-radius: 10px;
+		margin-bottom: 5px;
+	}
+
+	form {
+		display: flex;
+	}
+
+	input {
+		flex-grow: 1;
+		margin-right: 10px;
+		padding: 10px;
+		border: 1px solid #ccc;
+		border-radius: 5px;
+	}
+
+	button {
+		background-color: #4caf50;
+		color: white;
+		border: none;
+		padding: 10px 15px;
+		border-radius: 5px;
+		cursor: pointer;
+	}
+	#loader {
+		position: relative;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+</style>
